3b782f949a7db9290611b4ae9be0d4c520cf53f3
SrivastavaRachit <rachitsrivastava789@gmail.com>
2025-08-05 16:17:14 +0530

    dzfx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Updated App.js
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { CartProvider } from "./CartContext/CartContext";
+import Home from "./pages/Home/Home";
+import CartPage from "./pages/CartPage/CartPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import ContactUs from "./pages/ContactUs/ContactUs";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
+import BookPage from "./pages/BookPage/BookPage";
+import MyOrders from "./components/MyOrders/MyOrders";
+import Checkout from "./components/Checkout/Checkout";
+import VerifyPaymentPage from "./pages/VerifyPaymentPage";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <CartProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/books" element={<BookPage />} />
+        <Route path="/orders/verify" element={<VerifyPaymentPage />} />
+        <Route path="/orders" element={<MyOrders />} />
+  <Route
+         path="/checkout"
+         element={
+           <ProtectedRoute>
+             <Checkout />
+           </ProtectedRoute>
+        }
+       />
 
-export default App
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </CartProvider>
+  );
+};
+
+export default App;
