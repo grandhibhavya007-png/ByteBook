6705edb37be2c5c0cddfae07289f5c3336430336
Grandhi Bhavya <grandhibhavya007@gmail.com>
2026-07-15 14:50:49 +0530

    Fix GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/ByteBook/'
+  base: '/ByteBook/',
 })
-=======
-  base: '/Bytebook/'
-})
-
->>>>>>> f8b3c9f3a34c7b8534965929b312d94f69f28fc2
